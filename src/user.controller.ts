989e290471bd17a55f5515a47a85f7f3812be0f6
Marco <mornsophannamis@gmail.com>
2023-08-22 13:38:47 +0700

    communicate gate-way with user service

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Inject, UseGuards } from '@nestjs/common';
+import { 
+  Body, 
+  Controller, 
+  Delete, 
+  Get, 
+  Inject, 
+  Param, 
+  Post, 
+  Put, 
+  UseGuards 
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { AuthGuard } from './auth.guard';
 
@@ -10,5 +20,28 @@ export class UserController {
   @Get()
   getAllUsers() {
     return this.client.send({ cmd: 'get_users' }, {});
+  }
+
+  @Get('/:id')
+  getUser(@Param('id') id: number) {
+    return this.client.send({ cmd: 'get_user' }, id);
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: any) {
+    return this.client.send({ cmd: 'create_user' }, createUserDto);
+  }
+
+  @Put('/:id')
+  updateUser(
+    @Param('id') id: number,
+    @Body() updateUserDto: any
+  ) {
+    return this.client.send({ cmd: 'update_user' }, {id, ...updateUserDto});
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.client.send({ cmd: 'delete_user' }, +id);
   }
 }
