6fd034b18d3640d27d7bdf73eed95fcb8f5db4f8
Marco <mornsophannamis@gmail.com>
2023-08-22 16:25:09 +0700

    some modification on auth controller

--- src/auth.controller.ts
+++ src/auth.controller.ts
@@ -23,4 +23,25 @@ export class AuthController {
     const userAgent = req.headers['user-agent'];
     return this.client.send({ cmd: 'auth_login' }, { username, password, ip, userAgent });
   }
+
+  @Post('change_password')
+  changePassword(
+    @Headers('oldPassword') oldPassword: string,
+    @Headers('newPassword') newPassword: string
+  ) {
+    return this.client.send({ cmd: 'change_password' }, { oldPassword, newPassword });
+  }
+
+  @Post('reset_password')
+  resetPassword(
+    @Headers('newPassword') newPassword: string,
+    @Headers('confirmPassword') confirmPassword: string
+  ) {
+    return this.client.send({ cmd: 'reset_password' }, { newPassword, confirmPassword });
+  }
+
+  @Post('logout')
+  logout() {
+    return this.client.send({ cmd: 'logout' }, {});
+  }
 }
